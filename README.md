2bbc4a26aadd1bce9c93da8539159978d01b4b32
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-03-04 17:19:02 +0100

    Add hello route with query params

---
+++ README.md
@@ -0,0 +1,6 @@
+https://www.youtube.com/watch?v=XZtlD_m59sM
+
+# watch changes
+
+- watch change: `cargo watch -q -c -w src/ -x run`
+- watch tests: `cargo watch -q -c -w tests/ -x "test -q quick_dev -- --nocapture"`
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,29 @@
 use std::net::SocketAddr;
 
-use axum::{response::Html, routing::get, Router};
+use axum::{
+    extract::Query,
+    response::{Html, IntoResponse},
+    routing::get,
+    Router,
+};
+use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route(
-        "/hello",
-        get(|| async { Html("Hellof <strong>World!!!</strong>") }),
-    );
+    let routes_hello = Router::new().route("/hello", get(handler_hello));
     let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
     println!("->> Listening on {}", addr);
     let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, routes_hello).await.unwrap();
+}
+
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("Hello <strong>{name}!!</strong>"))
 }
