5a68811cb5fa745eddc50847ca23224d36eadabe
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-03-08 15:03:42 +0100

    Add context extractor

--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,21 +1,56 @@
-use axum::{body::Body, extract::Request, middleware::Next, response::Response};
+use async_trait::async_trait;
+
+use axum::{
+    body::Body,
+    extract::{FromRequestParts, Request},
+    http::request::Parts,
+    middleware::Next,
+    response::Response,
+    RequestPartsExt,
+};
 use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
-use crate::{web::AUTH_TOKEN, Error, Result};
+use crate::{ctx::Ctx, web::AUTH_TOKEN, Error, Result};
 
-pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -> Result<Response> {
-    println!("->> {:12} - mw_require_auth", "MIDDLEWARE");
-    let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+/// This function should take extractors as first arguments and next as a final arg.
+/// Extractors must implement FromRequestParts. Here Request is provided by axum and
+/// Ctx is custom made
+/// There are 3 ways to inject an extractor: directly, as a result or an option:
+/// - If i pass Ctx directly, then the method is called before passing the ctx to the
+/// middleware
+/// - if I pass it as a result, the middleware is called first and I can call `ctx?;`
+/// so that it throws an error if it fails
+/// - if I pass it as an option, it will be None on failure but it won't stop the
+/// execution of the middleware
+/// Check around 55:00
+pub async fn mw_require_auth(ctx: Result<Ctx>, req: Request<Body>, next: Next) -> Result<Response> {
+    println!("->> {:12} - mw_require_auth - {ctx:?}", "MIDDLEWARE");
 
-    // parse token
-    let (user_id, exp, sign) = auth_token
-        .ok_or(Error::AuthFailNoAuthTokenCookie)
-        .and_then(parse_token)?;
-
-    // TODO: Token components validation
+    ctx?;
 
     Ok(next.run(req).await)
+}
+
+// impl of the trait for ctx so that we can use it as an extractor
+#[async_trait]
+impl<S: Send + Sync> FromRequestParts<S> for Ctx {
+    type Rejection = Error;
+
+    async fn from_request_parts(parts: &mut Parts, _state: &S) -> Result<Self> {
+        println!("->> {:<12} - Ctx", "EXTRACTOR");
+        let cookies = parts.extract::<Cookies>().await.unwrap();
+        let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
+
+        // parse token
+        let (user_id, exp, sign) = auth_token
+            .ok_or(Error::AuthFailNoAuthTokenCookie)
+            .and_then(parse_token)?;
+
+        // TODO: Token components validation
+
+        Ok(Ctx::new(user_id))
+    }
 }
 
 fn parse_token(token: String) -> Result<(u64, String, String)> {
