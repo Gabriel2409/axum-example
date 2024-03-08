282a875f9cb4c1ba6f5dd2176d4cd1fb6e60a29e
Gabriel2409 <gabriel.cournelle@gmail.com>
2024-03-08 10:30:17 +0100

    Parse token

--- src/error.rs
+++ src/error.rs
@@ -10,6 +10,7 @@ pub type Result<T> = core::result::Result<T, Error>;
 pub enum Error {
     LoginFail,
     AuthFailNoAuthTokenCookie,
+    AuthFailTokenWrongFormat,
     TicketDeleteFailIdNotFound { id: u64 },
 }
 
--- src/web/mw_auth.rs
+++ src/web/mw_auth.rs
@@ -1,4 +1,5 @@
 use axum::{body::Body, extract::Request, middleware::Next, response::Response};
+use lazy_regex::regex_captures;
 use tower_cookies::Cookies;
 
 use crate::{web::AUTH_TOKEN, Error, Result};
@@ -7,8 +8,26 @@ pub async fn mw_require_auth(cookies: Cookies, req: Request<Body>, next: Next) -
     println!("->> {:12} - mw_require_auth", "MIDDLEWARE");
     let auth_token = cookies.get(AUTH_TOKEN).map(|c| c.value().to_string());
 
-    // TODO: real auth-token parsing and validation
-    auth_token.ok_or(Error::AuthFailNoAuthTokenCookie)?;
+    // parse token
+    let (user_id, exp, sign) = auth_token
+        .ok_or(Error::AuthFailNoAuthTokenCookie)
+        .and_then(parse_token)?;
+
+    // TODO: Token components validation
 
     Ok(next.run(req).await)
+}
+
+fn parse_token(token: String) -> Result<(u64, String, String)> {
+    let (_whole, user_id, exp, sign) = regex_captures!(
+        r#"^user-(\d+)\.(.+)\.(.+)"#, // literal regex
+        &token
+    )
+    .ok_or(Error::AuthFailTokenWrongFormat)?;
+
+    let user_id: u64 = user_id
+        .parse()
+        .map_err(|_| Error::AuthFailTokenWrongFormat)?;
+
+    Ok((user_id, exp.to_string(), sign.to_string()))
 }
